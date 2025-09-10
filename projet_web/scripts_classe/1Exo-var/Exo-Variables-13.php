747a92d942b69517e5e5c44ae3e0193afb185959
gg <georgevanderveen@yahoo.fr>
2025-09-10 19:14:12 +0200

    oublie exo 9 variables

--- projet_web/scripts_classe/1Exo-var/Exo-Variables-13.php
+++ projet_web/scripts_classe/1Exo-var/Exo-Variables-13.php
@@ -1,3 +1,3 @@
 <?php
 $films = ['Blade Runner 2049', 'D2', 'Dune', 'Minority Report', 'Gattaca', 'Interstellar'];
-var_dump(array_search('Dune', $films));
+var_dump(in_array('Dune', $films));
---
+++ projet_web/scripts_classe/2Exo-cond/test.php
@@ -0,0 +1,4 @@
+<?php
+$anneeDeMariage = readline('Veuillez entrer le nombre 5 :');
+$a = "5";
+echo (gettype($anneeDeMariage == $a));
