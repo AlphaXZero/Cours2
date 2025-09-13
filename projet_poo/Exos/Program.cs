154b671e6e1a163853207590d0d59bfb67e6b23b
gg <georgevanderveen@yahoo.fr>
2025-09-13 12:48:18 +0200

    fin exo 2ème cours

--- projet_poo/Exos/Program.cs
+++ projet_poo/Exos/Program.cs
@@ -7,8 +7,10 @@ class Program
         // Exercice1.Run();
         // Exercice2.Run();
         // Exercice3.Run();
-        Exercice4.Run();
-
+        // Exercice4.Run();
+        // Exercice5.Run();
+        // Exercice6.Run();
+        // Exercice7.Run();
     }
 }
 
@@ -17,9 +19,9 @@ class Exercice1
     public static void Run()
     {
         Console.WriteLine("Nom ?");
-        string name = Console.ReadLine();
+        string? name = Console.ReadLine();
         Console.WriteLine("Age ?");
-        string age = Console.ReadLine();
+        string? age = Console.ReadLine();
         Console.WriteLine("Vous êtes " + name + " et vous avez " + age);
     }
 }
@@ -39,7 +41,7 @@ class Exercice3
     public static void Run()
     {
         Console.WriteLine("Entrez une valeur décimale");
-        string usr_input = Console.ReadLine();
+        string? usr_input = Console.ReadLine();
         double val = double.Parse(usr_input);
         Console.WriteLine($"la valeur arrondie est {Math.Round(val, 2)}");
     }
@@ -51,5 +53,33 @@ class Exercice4
     {
         int day_amount = 123;
         Console.WriteLine($"{day_amount} jours font {day_amount * 24} heures soit {day_amount * 24 * 60} minutes ou {day_amount * 24 * 60 * 60} secondes");
+    }
+}
+
+class Exercice5
+{
+    public static void Run()
+    {
+        Console.WriteLine("Entrez un nombre à doubler");
+        string? nbrToDubble = Console.ReadLine();
+        Console.WriteLine((int.Parse(nbrToDubble) * 2).ToString());
+    }
+}
+
+class Exercice6
+{
+    public static void Run()
+    {
+        Console.WriteLine("Entrez un nombre décimal");
+        Console.WriteLine(float.TryParse(Console.ReadLine(), out float decimalNbr) ? Convert.ToInt32(decimalNbr) : "Entrée invalide");
+    }
+}
+
+class Exercice7
+{
+    public static void Run()
+    {
+        int celTemp = 20;
+        Console.WriteLine($"{celTemp}°C fait {celTemp * (float)9 / (float)5 + 32}° en Fahrenheit");
     }
 }
